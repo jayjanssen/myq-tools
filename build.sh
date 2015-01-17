450eb7df12d9d0589f4fd7d9ad917a8cedc2bab1
Jay Janssen <jay.janssen@percona.com>
2015-01-17 18:15:52 -0500

    testing releases

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 go test -bench=. ./...
 
-gox -build-toolchain
+gox -os="linux darwin freebsd" -arch="386 amd64 arm" -build-toolchain
 gox -os="linux darwin freebsd" -arch="386 amd64 arm" -output="bin/myq_status.{{.OS}}-{{.Arch}}"
 tar cvzf myq_tools.tgz bin/*
