d338f809bc32aca15df72da49cb219eb2c77cc11
Jay Janssen <jay.janssen@percona.com>
2015-01-17 17:57:59 -0500

    try building the toolchain for gox + travis

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 go test -bench=. ./...
+
+gox -build-toolchain
 gox -os="linux darwin freebsd" -arch="386 amd64 arm" -output="bin/myq_status.{{.OS}}-{{.Arch}}"
 tar cvzf myq_tools.tgz bin/*
