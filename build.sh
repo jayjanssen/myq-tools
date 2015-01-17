39241397ea2b40bdda9c78a03fbed80dcaff472a
Jay Janssen <jay.janssen@percona.com>
2015-01-17 17:16:16 -0500

    Change to gox to try to get travis to cross-compile

--- build.sh
+++ build.sh
@@ -1,15 +1,3 @@
 #!/bin/sh
 
-# Full build on the local OS
-make
-
-# Linux builds
-GOOS=linux GOARCH=386 make build
-GOOS=linux GOARCH=amd64 make build
-GOOS=linux GOARCH=arm make build
-
-# Other OSes
-GOOS=darwin GOARCH=amd64 make build
-GOOS=freebsd GOARCH=amd64 make build
-GOOS=openbsd GOARCH=amd64 make build
-GOOS=windows GOARCH=amd64 make build
+gox -os="linux darwin freebsd" -arch="386 amd64 arm" -output="bin/myq_status.{{.OS}}-{{.Arch}}"
