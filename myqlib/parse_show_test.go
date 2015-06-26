c7268dfc160279b67760ffff541dec76836a8e13
Jay Janssen <jay.janssen@percona.com>
2015-06-26 14:20:16 -0400

    Add testing for Batch output parsing

--- myqlib/parse_show_test.go
+++ myqlib/parse_show_test.go
@@ -3,6 +3,7 @@ package myqlib
 import (
 	"testing"
 	"time"
+	// "fmt"
 )
 
 func TestSingleSample(t *testing.T) {
@@ -75,6 +76,63 @@ func TestManySamples(t *testing.T) {
 	}
 
 	checksamples(t, samples, 220)
+}
+
+func TestSingleBatchSample(t *testing.T) {
+	l := FileLoader{loaderInterval(1 * time.Second), "../testdata/mysql.single", ""}
+	samples, err := l.getStatus()
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Check some types on some known metrics to verify autodetection
+	sample := <-samples
+	typeTests := map[string]string{
+		"connections":                "int64",
+		"compression":                "string",
+		"wsrep_local_send_queue_avg": "float64",
+		"binlog_snapshot_file":       "string",
+	}
+
+	for varname, expectedtype := range typeTests {
+		i, ierr := sample.getInt(varname)
+		if ierr == nil {
+			if expectedtype != "int64" {
+				t.Fatal("Found integer, expected", expectedtype, "for", varname, "value: `", i, "`")
+			} else {
+				continue
+			}
+		}
+
+		f, ferr := sample.getFloat(varname)
+		if ferr == nil {
+			if expectedtype != "float64" {
+				t.Fatal("Found float, expected", expectedtype, "for", varname, "value: `", f, "`")
+			} else {
+				continue
+			}
+		}
+
+		s, serr := sample.getString(varname)
+		if serr == nil {
+			if expectedtype != "string" {
+				t.Fatal("Found string, expected", expectedtype, "for", varname, "value: `", s, "`")
+			} else {
+				continue
+			}
+		}
+	}
+}
+
+func TestTwoBatchSamples(t *testing.T) {
+	l := FileLoader{loaderInterval(1 * time.Second), "../testdata/mysql.two", ""}
+	samples, err := l.getStatus()
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	checksamples(t, samples, 2)
 }
 
 func checksamples(t *testing.T, samples chan MyqSample, expected int) {
