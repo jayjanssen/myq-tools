16d9b2c755a3e5d35e55814e0266e695702bfe3c
Jay Janssen <jayj@squareup.com>
2022-07-22 14:33:58 -0400

    added commands view and special column

--- viewer/types.go
+++ viewer/types.go
@@ -59,6 +59,13 @@ func (svl *StateViewerList) UnmarshalYAML(value *yaml.Node) error {
 				return err
 			}
 			newlist = append(newlist, c)
+		case `SortedExpandedCounts`:
+			c := SortedExpandedCountsCol{}
+			err = content.Decode(&c)
+			if err != nil {
+				return err
+			}
+			newlist = append(newlist, c)
 		default:
 			return fmt.Errorf("invalid column type: %s", typeobj.Type)
 		}
